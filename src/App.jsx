389e90514d7a880238e9a86c1adf488454d01454
kamrul397 <kamrulislam25262800@gmail.com>
2025-09-19 12:30:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React World....</h1>
+      <Countries />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Countries() {
+  return (
+    <div>
+      <h2>Countries Component</h2>
+    </div>
+  );
+}
