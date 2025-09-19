a92c71cc78f8c566770f1d0f96c21ee955797dfa
kamrul397 <kamrulislam25262800@gmail.com>
2025-09-19 12:56:44 +0600

    jummar age

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import "./App.css";
 import Countries from "./components/Countries/Countries";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+).then((res) => res.json());
 
 function App() {
   return (
     <>
       <h1>Vite + React World....</h1>
-      <Countries />
+      <Suspense fallback={<h2>Loading Countries...</h2>}>
+        <Countries countriesPromise={countriesPromise} />
+      </Suspense>
     </>
   );
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { use } from "react";
 
-export default function Countries() {
+export default function Countries({ countriesPromise }) {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
   return (
     <div>
-      <h2>Countries Component</h2>
+      <h2>Countries: {countries.length}</h2>
     </div>
   );
 }
